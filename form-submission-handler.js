14864b41f91443efd8fc8240e4b5c4667018c2b4
Felipe Toledo <analisegrafica@gmail.com>
2023-01-25 19:20:29 -0300

    JS undo remove form fields

--- form-submission-handler.js
+++ form-submission-handler.js
@@ -70,7 +70,7 @@
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
           form.reset();
-          var formElements = form.querySelector(".form-control")
+          var formElements = form.querySelector(".form-elements")
           if (formElements) {
             formElements.style.display = "none"; // hide form
           }
